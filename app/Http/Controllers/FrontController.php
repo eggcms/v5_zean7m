33af1f186a70b18c287f96cd4a3854085700b617
omyimplus_oat <omyimplusoat@gmail.com>
2020-02-29 20:10:48 +0700

    update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -22,8 +22,8 @@ class FrontController extends Controller
         $dataxSet = [];
         $im=0;
         if ($max_tstep > 0) {
-			$im++;
 			foreach($tsteps as $ttsx) {
+				$im++;
 				$av = User::where('id',$ttsx->uid)->first();
 				if ($ttsx->team1w == 0) { $ttsx->team1w='black'; }
 				elseif ($ttsx->team1w == 1) { $ttsx->team1w='red'; }
